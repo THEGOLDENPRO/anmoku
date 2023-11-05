017a338210800abb72d430bd0d6ef8170da0e0fb
Dev Goldy <jassim7256@gmail.com>
2023-11-05 02:44:03 +0000

    feat: added classes for anime character and renamed a bunch of types

--- anmoku/objects/anime.py
+++ anmoku/objects/anime.py
@@ -2,9 +2,22 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ..typing.jikan import AnimeData, FullAnimeData, AnimeCharactersData, JikanResponseData
+    from typing import List
+    from ..typing.jikan import (
+        AnimeData, 
+        FullAnimeData, 
+        AnimeCharacterData, 
+        JikanResponseData, 
 
-from dataclasses import dataclass
+        # Temporary
+        # -----------
+        PartialPerson,
+        PartialCharacter
+    )
+
+    from ..typing.jikan.anime import VoiceActorData
+
+from dataclasses import dataclass, field
 
 __all__ = (
     "Anime", 
@@ -26,7 +39,36 @@ class FullAnime(Anime):
 
 
 @dataclass
+class VoiceActor():
+    data: VoiceActorData
+
+    person: PartialPerson = field(init = False) # TODO: Change this to actual person class.
+    language: str = field(init = False)
+
+    def __post_init__(self):
+        self.person = self.data.get("person")
+        self.language = self.data.get("language")
+
+@dataclass
+class AnimeCharacter():
+    data: AnimeCharacterData
+
+    character: PartialCharacter = field(init = False) # TODO: Change this to actual character class.
+    role: str = field(init = False)
+    voice_actors: List[VoiceActor] = field(init = False)
+
+    def __post_init__(self):
+        self.character = self.data.get("character")
+        self.role = self.data.get("role")
+        self.voice_actors = [VoiceActor(actor) for actor in self.data.get("voice_actors", [])]
+
+@dataclass
 class AnimeCharacters():
     _get_endpoint = "/anime/{id}/characters"
 
-    data: JikanResponseData[AnimeCharactersData]
+    data: JikanResponseData[List[AnimeCharacterData]]
+
+    def __iter__(self):
+
+        for character in self.data["data"]:
+            yield AnimeCharacter(character)
--- anmoku/typing/jikan/anime.py
+++ anmoku/typing/jikan/anime.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 __all__ = (
     "AnimeData",
     "FullAnimeData",
-    "AnimeCharactersData"
+    "AnimeCharacterData"
 )
 
 # NOTE: This module is subject to change. I may move some types here to a central location to be used in other types.
@@ -50,21 +50,21 @@ class BroadcastData(TypedDict):
     timezone: str
     string: str
 
-class Entry(TypedDict):
+class EntryData(TypedDict):
     mal_id: int
     type: str
     name: str
     url: str
 
-class Relation(TypedDict):
+class RelationData(TypedDict):
     relation: str
-    entry: List[Entry]
+    entry: List[EntryData]
 
-class Theme(TypedDict):
+class ThemeData(TypedDict):
     openings: List[str]
     endings: List[str]
 
-class ExternalSource(TypedDict):
+class ExternalSourceData(TypedDict):
     name: str
     url: str
 
@@ -98,30 +98,30 @@ class AnimeData(TypedDict):
     season: Literal["summer", "fall"] | Any # TODO: Find the rest of these then remove Any.
     year: int
     broadcast: BroadcastData
-    producers: List[Entry]
-    licensors: List[Entry]
-    studios: List[Entry]
-    genres: List[Entry]
-    explicit_genres: List[Entry]
-    themes: List[Entry]
-    demographics: List[Entry]
+    producers: List[EntryData]
+    licensors: List[EntryData]
+    studios: List[EntryData]
+    genres: List[EntryData]
+    explicit_genres: List[EntryData]
+    themes: List[EntryData]
+    demographics: List[EntryData]
 
 @final
 class FullAnimeData(AnimeData): # TODO: Finish this.
-    relations: List[Relation]
-    theme: Theme
-    external: List[ExternalSource]
-    streaming: List[ExternalSource]
+    relations: List[RelationData]
+    theme: ThemeData
+    external: List[ExternalSourceData]
+    streaming: List[ExternalSourceData]
 
 
 @final
-class VoiceActor(TypedDict):
+class VoiceActorData(TypedDict):
     person: PartialPerson
     language: str
 
 
 @final
-class AnimeCharactersData(TypedDict):
+class AnimeCharacterData(TypedDict):
     character: PartialCharacter
     role: str
-    voice_actors: List[VoiceActor]
+    voice_actors: List[VoiceActorData]
