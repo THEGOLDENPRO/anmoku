22c9be4d52749e5978bd45f138c25953b257f52a
Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
2023-11-11 10:30:20 -0800

    feat: partial work on Anime class

--- anmoku/objects/anime/anime.py
+++ anmoku/objects/anime/anime.py
@@ -2,15 +2,18 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from typing import Optional
+
     from ...typing.jikan import (
         AnimeData, 
         FullAnimeData, 
-        JikanResponseData
+        JikanResponseData,
     )
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from ..base import JikanObject
+from ..image import Image
 
 __all__ = (
     "Anime", 
@@ -22,6 +25,23 @@ class Anime(JikanObject):
     _get_endpoint = "/anime/{id}"
 
     data: JikanResponseData[AnimeData]
+
+    id: int = field(init = False)
+    url: str = field(init = False)
+    image: Image = field(init = False)
+    # TODO: trailer
+    approved: bool = field(init = False)
+    # TODO: titles, type (will be an enum)
+    source: Optional[str] = field(init = False)
+
+    def __post_init__(self):
+        anime = self.data["data"]
+
+        self.id = anime["mal_id"]
+        self.url = anime["url"]
+        self.image = Image(anime["images"])
+        self.approved = anime["approved"]
+        self.source = anime["source"]
 
 @dataclass
 class FullAnime(Anime):
